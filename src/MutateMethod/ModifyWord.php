194dbc0a5a5dd1ca68c88d87ced7da62d1e449d5
Martin Price <martin.price@emailcenteruk.com>
2015-03-31 11:50:51 +0100

    Add character swapping logic to the ModifyWord mutation method, should happen ~50% of the time instead of character changing

--- src/MutateMethod/ModifyWord.php
+++ src/MutateMethod/ModifyWord.php
@@ -31,9 +31,20 @@ class ModifyWord implements MutateMethodInterface
         $charChanges = ceil($entropy * ($valueLength / 2));
         $alphabet    = $this->alphabet;
         for ($i = 0; $i < $charChanges; $i++) {
-            $charIndex = mt_rand(0, $valueLength - 1);
-            $newChar   = $alphabet[array_rand($alphabet)];
-            $value[$charIndex] = $newChar;
+            if (mt_rand(0, 1)) {
+                // change character
+                $charIndex = mt_rand(0, $valueLength - 1);
+                $newChar   = $alphabet[array_rand($alphabet)];
+                $value[$charIndex] = $newChar;
+            } else {
+                // swap characters
+                $charIndexFrom = mt_rand(0, $valueLength - 1);
+                $charIndexTo   = mt_rand(0, $valueLength - 1);
+
+                $tmp = $value[$charIndexFrom];
+                $value[$charIndexFrom] = $value[$charIndexTo];
+                $value[$charIndexTo]   = $tmp;
+            }
         }
         return $value;
     }
