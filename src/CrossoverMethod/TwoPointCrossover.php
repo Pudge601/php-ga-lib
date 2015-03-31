6ca4cbde0a6a78b6bad3ef80cda9a620d34b5a81
Martin Price <martin.price@emailcenteruk.com>
2015-03-31 14:39:20 +0100

    Return two children from the two point crossover method

--- src/CrossoverMethod/TwoPointCrossover.php
+++ src/CrossoverMethod/TwoPointCrossover.php
@@ -18,11 +18,18 @@ class TwoPointCrossover implements CrossoverMethodInterface
         $crossoverPoint1 = mt_rand(0, ceil($valueCount / 2));
         $crossoverPoint2 = mt_rand(ceil($valueCount / 2), $valueCount - 1);
 
-        return [array_merge(
-            array_slice($parentA, 0, $crossoverPoint1),
-            array_slice($parentB, $crossoverPoint1, $crossoverPoint2 - $crossoverPoint1),
-            array_slice($parentA, $crossoverPoint2)
-        )];
+        return [
+            array_merge(
+                array_slice($parentA, 0, $crossoverPoint1),
+                array_slice($parentB, $crossoverPoint1, $crossoverPoint2 - $crossoverPoint1),
+                array_slice($parentA, $crossoverPoint2)
+            ),
+            array_merge(
+                array_slice($parentB, 0, $crossoverPoint1),
+                array_slice($parentA, $crossoverPoint1, $crossoverPoint2 - $crossoverPoint1),
+                array_slice($parentB, $crossoverPoint2)
+            )
+        ];
     }
 
 }
